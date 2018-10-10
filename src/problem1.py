0ec7497cd1064c9c068729ca72a7d437ac3667a2
bressmjm <bressmjm@rose-hulman.edu>
2018-10-09 20:23:08 -0400

    #1 in progress

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Joshua Bressman.  October 2018.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -48,7 +48,7 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem1a()
+    run_test_problem1a()
     # run_test_problem1b()
     # run_test_problem1c()
     # run_test_problem1d()
@@ -183,6 +183,10 @@ def run_test_problem1a():
 
 
 def problem1a(strings):
+    wordlengthseq = []
+    for k in range(len(strings)):
+        wordlengthseq = wordlengthseq + [len(strings[k])]
+    return wordlengthseq
     """
     What comes in:
       -- a sequence of strings
@@ -218,7 +222,7 @@ def problem1a(strings):
       :rtype: list of int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # Done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
